c877c4f3246a7acdd1d45370d4c260128f47b1eb
kutaui <kutaybekleric@gmail.com>
2024-03-29 15:34:15 +0300

    feat: add sqlc

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 	"github.com/kutaui/url-shortener/db"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	db.RedisClient()
+var ctx = context.Background()
 
+func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	q := db.New(conn)
+	godotenv.Load()
+	rdb := db.RedisClient()
 	router := http.NewServeMux()
 
 	server := &http.Server{
