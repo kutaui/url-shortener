c9f1db4eb8728e2620dbbb58f4ab9a0f5a342efe
kutaui <kutaybekleric@gmail.com>
2024-04-14 14:00:53 +0300

    feat: setup redis

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	db "github.com/kutaui/url-shortener/db/sqlc"
 	"github.com/kutaui/url-shortener/handlers"
 	"github.com/kutaui/url-shortener/utils"
+	"github.com/redis/go-redis/v9"
 )
 
 var ctx = context.Background()
@@ -30,6 +31,13 @@ func main() {
 	q := db.New(conn)
 	router := http.NewServeMux()
 
+	opts, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	rdb := redis.NewClient(opts)
+
 	// find a native way for authmiddleware or route grouping
 
 	router.HandleFunc("GET /link", utils.AuthMiddleware(handlers.GetLink(q)))
@@ -42,7 +50,7 @@ func main() {
 
 	router.HandleFunc("POST /register", handlers.Register(q))
 	router.HandleFunc("POST /login", handlers.Login(q))
-	router.HandleFunc("GET /{code}", handlers.Redirect(q))
+	router.HandleFunc("GET /{code}", handlers.Redirect(q, rdb))
 
 	server := &http.Server{
 		Addr:    ":8080",
