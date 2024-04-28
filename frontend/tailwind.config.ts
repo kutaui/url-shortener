390309cbb8e619b66cab44b435fa66e06ed62d48
kutaui <kutaybekleric@gmail.com>
2024-04-28 11:49:49 +0300

    feat: auth form

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from 'tailwindcss'
 
+const {
+	default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette')
+
 const config = {
 	darkMode: ['class'],
 	content: [
@@ -12,7 +16,7 @@ const config = {
 	theme: {
 		screens: {
 			md: '700px',
-			"sm": '400px',
+			sm: '400px',
 			'2xl': '1400px',
 		},
 		container: {
@@ -20,7 +24,7 @@ const config = {
 			padding: '2rem',
 			screens: {
 				md: '700px',
-				"400": '400px',
+				'400': '400px',
 				'2xl': '1400px',
 			},
 		},
@@ -88,9 +92,23 @@ const config = {
 				'accordion-up': 'accordion-up 0.2s ease-out',
 				'meteor-effect': 'meteor 5s linear infinite',
 			},
+			boxShadow: {
+				input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
+	plugins: [require('tailwindcss-animate'), addVariablesForColors],
 } satisfies Config
 
 export default config
+
+function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme('colors'))
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	)
+
+	addBase({
+		':root': newVars,
+	})
+}
